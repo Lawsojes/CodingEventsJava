71afd8aee5ac73c4c13cb516a8c0282661dfcfee
Jesse Lawson <jesse.t.lawson@gmail.com>
2023-10-28 17:27:57 -0500

    completed up to creating model class

--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
